db5f09b9bedc62e2e16de1a145fee1339741b638
su apt install Kuma <maxxozou@gmail.com>
2023-02-08 08:11:16 +0100

    Ver 1.6

--- admin/deconnexion.php
+++ admin/deconnexion.php
@@ -8,12 +8,12 @@
     $log->execute([$_SESSION['utilisateur'][5], $textLog, $dateLog]);
 
     $_SESSION['utilisateur'] = array();
+    
     $_SESSION['patient'] = array();
     $_SESSION['personneConfiance'] = array();
     $_SESSION['personnePrevenir'] = array();
     $_SESSION['hospitalisation'] = array();
     $_SESSION['couvertureSociale'] = array();
-
     $_SESSION['creer_admission'] = array();
 
     session_destroy();
