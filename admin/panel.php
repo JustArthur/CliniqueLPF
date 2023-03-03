b65843e3fc9de0b347768e7894238c82f4f429c9
su apt install kuma <maxxozou@gmail.com>
2023-03-03 09:04:53 +0100

    Ver 1.11

--- admin/panel.php
+++ admin/panel.php
@@ -7,12 +7,16 @@
         exit;
     }
 
+    $dateAujourdhui = date('Y-m-d');
+
+    //-- Vide les SESSIONS pour une pré-admission ----------------
     $_SESSION['patient'] = array();
     $_SESSION['personneConfiance'] = array();
     $_SESSION['personnePrevenir'] = array();
     $_SESSION['hospitalisation'] = array();
     $_SESSION['couvertureSociale'] = array();
 
+    //-- Pour empêcher l'ouverture des pages suivantes sans passer par les requises ----------------
     $_SESSION['creer_admission'] = array(
         false, //0
         false, //1
@@ -21,82 +25,7 @@
         false //4
     );
 
-    $anneeChoix = date('Y');
-    $semChoix = date('W');
-
-    $timeStampPremierJanvier = strtotime($anneeChoix . '-01-01');
-    $jourPremierJanvier = date('w', $timeStampPremierJanvier);
-    
-    //-- Recherche du N° de semaine du 1er janvier -------------------
-    $numSemainePremierJanvier = date('W', $timeStampPremierJanvier);
-    
-    //-- Nombre à ajouter en fonction du numéro précédent ------------
-    $decallage = ($numSemainePremierJanvier == 1) ? $semChoix - 1 : $semChoix;
-
-    //-- Timestamp du jour dans la semaine recherchée ----------------
-    $timeStampDate = strtotime('+' . $decallage . 'weeks', $timeStampPremierJanvier);
-    
-    //-- Recherche du lundi de la semaine en fonction de la ligne précédente ---------
-    $jourDebutSemaine = ($jourPremierJanvier == 1) ? date('Y-m-d', $timeStampDate) : date('Y-m-d', strtotime('last monday', $timeStampDate));
-    $jourFinSemaine = ($jourPremierJanvier == 1) ? date('Y-m-d', $timeStampDate) : date('Y-m-d',strtotime(' sunday', $timeStampDate));
-
-
-    //-- Recherche de la date du jour ----------------
-    $dateAujourdhui = date('Y-m-d');
-
-    //-- Requête pour les admission ou autres ----------------
-    $nbr_admission_today = $DB->prepare("SELECT * FROM operations WHERE dateOperation >= ? AND dateOperation <= ?");
-    $nbr_admission_today->execute([$jourDebutSemaine, $jourFinSemaine]);
-    $nbr_admission_today = $nbr_admission_today->rowcount();
-
-    $nbr_patient_today = $DB->prepare("SELECT * FROM preadmission WHERE dateAdmission = ?");
-    $nbr_patient_today->execute([$dateAujourdhui]);
-    $nbr_patient_today = $nbr_patient_today->rowcount();
-
-    $nbr_annule_today = $DB->prepare("SELECT p.idOperation, p.status, o.dateOperation FROM preadmission p INNER JOIN operations o ON p.idOperation = o.id WHERE p.status = 'Annulé' AND o.dateOperation >= ? AND o.dateOperation <= ?");
-    $nbr_annule_today->execute([$jourDebutSemaine, $jourFinSemaine]);
-    $nbr_annule_today = $nbr_annule_today->rowcount();
-
-    $nbr_encours_today = $DB->prepare("SELECT p.idOperation, p.status, o.dateOperation FROM preadmission p INNER JOIN operations o ON p.idOperation = o.id WHERE p.status = 'En cours' AND o.dateOperation >= ? AND o.dateOperation <= ?");
-    $nbr_encours_today->execute([$jourDebutSemaine, $jourFinSemaine]);
-    $nbr_encours_today = $nbr_encours_today->rowcount();
-
-    $nbr_termine_today = $DB->prepare("SELECT p.idOperation, p.status, o.dateOperation FROM preadmission p INNER JOIN operations o ON p.idOperation = o.id WHERE p.status = 'Terminé' AND o.dateOperation >= ? AND o.dateOperation <= ?");
-    $nbr_termine_today->execute([$jourDebutSemaine, $jourFinSemaine]);
-    $nbr_termine_today = $nbr_termine_today->rowcount();
-
-    //-- Esthetique du site si 0 ----------------
-    if($nbr_admission_today == 0) {
-        $nbr_admission_today = 'Aucune';
-    } else {
-        $nbr_admission_today = $nbr_admission_today;
-    }
-
-    if($nbr_patient_today == 0) {
-        $nbr_patient_today = 'Aucun';
-    } else {
-        $nbr_patient_today = $nbr_patient_today;
-    }
-
-    if($nbr_annule_today == 0) {
-        $nbr_annule_today = 'Aucune';
-    } else {
-        $nbr_annule_today = $nbr_annule_today;
-    }
-
-    if($nbr_termine_today == 0) {
-        $nbr_termine_today = 'Aucune';
-    } else {
-        $nbr_termine_today = $nbr_termine_today;
-    }
-
-    if($nbr_encours_today == 0) {
-        $nbr_encours_today = 'Aucune';
-    } else {
-        $nbr_encours_today = $nbr_encours_today;
-    }
-
-    //-- Récupères les 20 dernières pré-admissions du jour ----------------
+    //-- Récupères les pré-admissions prévu pour les 5 semaines à venir ----------------
     $dernierePrea = $DB->prepare("SELECT * FROM preadmission WHERE faitPar = ? AND dateAdmission = ? LIMIT 20");
     $dernierePrea->execute([$_SESSION['utilisateur'][5], $dateAujourdhui]);
     $dernierePreaFetch = $dernierePrea->fetchAll();
@@ -114,47 +43,21 @@
     <link rel="stylesheet" href="../style/navBar.css">
 
     <title>Bienvenue, <?= $_SESSION['utilisateur'][0] . ' ' . $_SESSION['utilisateur'][1] ?></title>
+
     <link rel="icon" href="../img/logo.png" type="image/icon type">
 </head>
 <body>
 
     <?php
+        //-- Appel le fichier navbar ----------------
         require_once('src/navbar.php');
     ?>
     
     <main>
-        <?php if($_SESSION['utilisateur'][3] == 1) {?>
-        <ul class="list-prea">
-            <a href="#" class="list-item">
-                <h3 class="titre">Pré-admissions</h3>
-                <p class="desc"><span class="color"><?= $nbr_admission_today ?></span> pré-admission prévu pour cette semaine.</p>
-            </a>
-
-            <a href="#" class="list-item">
-                <h3 class="titre">Patients</h3>
-                <p class="desc"><span class="color"><?= $nbr_patient_today ?></span> patients admis pour une future pré-admission.</p>
-            </a>
-
-            <a href="#" class="list-item">
-                <h3 class="titre">Annulé</h3>
-                <p class="desc"><span class="color"><?= $nbr_annule_today ?></span> pré-admissions annulé pour cette semaine.</p>
-            </a>
-
-            <a href="#" class="list-item">
-                <h3 class="titre">En cours</h3>
-                <p class="desc"><span class="color"><?= $nbr_encours_today ?></span> pré-admissions en cours pour cette semaine.</p>
-            </a>
-
-            <a href="#" class="list-item">
-                <h3 class="titre">Terminé</h3>
-                <p class="desc"><span class="color"><?= $nbr_termine_today ?></span> pré-admissions terminé pour cette semaine.</p>
-            </a>
-
-            <a href="ajout_admission" class="list-item">
-                <h3 class="titre">Créer une nouvelle pré-admission</h3>
-            </a>
-        </ul>
-        <?php } ?>
+        <?php if($_SESSION['utilisateur'][3] == 1) {
+            //-- Appel le fichier information ----------------
+            require_once('src/information.php');
+        } ?>
 
         <div class="profil">
             <div class="infos">
@@ -163,14 +66,14 @@
             </div>
 
             <div class="last-prea">
-                <h3>Vos 20 dernières pré-admissions du jour</h3>
+                <h3>Vos pré-admissions des 5 prochaines semaines</h3>
 
                 <ul class="list-prea-last">
                     <?php 
 
                         if($dernierePreaCount == 0) { ?>
                             <li class="list-item">
-                                <p>Aucune pré-admission réalisé aujourd'hui</p>
+                                <p>Aucune pré-admission prévu dans les 5 semaines</p>
                             </li>
                         <?php } else {
                             foreach($dernierePreaFetch as $preadmission) {
---
+++ admin/src/information.php
@@ -0,0 +1,107 @@
+<?php
+    $anneeChoix = date('Y');
+    $semChoix = date('W');
+
+    $timeStampPremierJanvier = strtotime($anneeChoix . '-01-01');
+    $jourPremierJanvier = date('w', $timeStampPremierJanvier);
+    
+    //-- Recherche du N° de semaine du 1er janvier -------------------
+    $numSemainePremierJanvier = date('W', $timeStampPremierJanvier);
+    
+    //-- Nombre à ajouter en fonction du numéro précédent ------------
+    $decallage = ($numSemainePremierJanvier == 1) ? $semChoix - 1 : $semChoix;
+
+    //-- Timestamp du jour dans la semaine recherchée ----------------
+    $timeStampDate = strtotime('+' . $decallage . 'weeks', $timeStampPremierJanvier);
+    
+    //-- Recherche du lundi de la semaine en fonction de la ligne précédente ---------
+    $jourDebutSemaine = ($jourPremierJanvier == 1) ? date('Y-m-d', $timeStampDate) : date('Y-m-d', strtotime('last monday', $timeStampDate));
+    $jourFinSemaine = ($jourPremierJanvier == 1) ? date('Y-m-d', $timeStampDate) : date('Y-m-d',strtotime(' sunday', $timeStampDate));
+
+
+    //-- Recherche de la date du jour ----------------
+    $dateAujourdhui = date('Y-m-d');
+
+    //-- Requête pour les admission ou autres ----------------
+    $nbr_admission_week = $DB->prepare("SELECT * FROM operations WHERE dateOperation >= ? AND dateOperation <= ?");
+    $nbr_admission_week->execute([$jourDebutSemaine, $jourFinSemaine]);
+    $nbr_admission_week = $nbr_admission_week->rowcount();
+
+    $nbr_patient_week = $DB->prepare("SELECT * FROM preadmission WHERE dateAdmission = ?");
+    $nbr_patient_week->execute([$dateAujourdhui]);
+    $nbr_patient_week = $nbr_patient_week->rowcount();
+
+    $nbr_annule_week = $DB->prepare("SELECT p.idOperation, p.status, o.dateOperation FROM preadmission p INNER JOIN operations o ON p.idOperation = o.id WHERE p.status = 'Annulé' AND o.dateOperation >= ? AND o.dateOperation <= ?");
+    $nbr_annule_week->execute([$jourDebutSemaine, $jourFinSemaine]);
+    $nbr_annule_week = $nbr_annule_week->rowcount();
+
+    $nbr_encours_week = $DB->prepare("SELECT p.idOperation, p.status, o.dateOperation FROM preadmission p INNER JOIN operations o ON p.idOperation = o.id WHERE p.status = 'En cours' AND o.dateOperation >= ? AND o.dateOperation <= ?");
+    $nbr_encours_week->execute([$jourDebutSemaine, $jourFinSemaine]);
+    $nbr_encours_week = $nbr_encours_week->rowcount();
+
+    $nbr_termine_week = $DB->prepare("SELECT p.idOperation, p.status, o.dateOperation FROM preadmission p INNER JOIN operations o ON p.idOperation = o.id WHERE p.status = 'Terminé' AND o.dateOperation >= ? AND o.dateOperation <= ?");
+    $nbr_termine_week->execute([$jourDebutSemaine, $jourFinSemaine]);
+    $nbr_termine_week = $nbr_termine_week->rowcount();
+
+    //-- Esthetique du site si 0 ----------------
+    if($nbr_admission_week == 0) {
+        $nbr_admission_week = 'Aucune';
+    } else {
+        $nbr_admission_week = $nbr_admission_week;
+    }
+
+    if($nbr_patient_week == 0) {
+        $nbr_patient_week = 'Aucun';
+    } else {
+        $nbr_patient_week = $nbr_patient_week;
+    }
+
+    if($nbr_annule_week == 0) {
+        $nbr_annule_week = 'Aucune';
+    } else {
+        $nbr_annule_week = $nbr_annule_week;
+    }
+
+    if($nbr_termine_week == 0) {
+        $nbr_termine_week = 'Aucune';
+    } else {
+        $nbr_termine_week = $nbr_termine_week;
+    }
+
+    if($nbr_encours_week == 0) {
+        $nbr_encours_week = 'Aucune';
+    } else {
+        $nbr_encours_week = $nbr_encours_week;
+    }
+?>
+
+<ul class="list-prea">
+    <a href="#" class="list-item">
+        <h3 class="titre">Pré-admissions</h3>
+        <p class="desc"><span class="color"><?= $nbr_admission_week ?></span> pré-admission prévu pour cette semaine.</p>
+    </a>
+
+    <a href="#" class="list-item">
+        <h3 class="titre">Patients</h3>
+        <p class="desc"><span class="color"><?= $nbr_patient_week ?></span> patients admis pour une future pré-admission.</p>
+    </a>
+
+    <a href="#" class="list-item">
+        <h3 class="titre">Annulé</h3>
+        <p class="desc"><span class="color"><?= $nbr_annule_week ?></span> pré-admissions annulé pour cette semaine.</p>
+    </a>
+
+    <a href="#" class="list-item">
+        <h3 class="titre">En cours</h3>
+        <p class="desc"><span class="color"><?= $nbr_encours_week ?></span> pré-admissions en cours pour cette semaine.</p>
+    </a>
+
+    <a href="#" class="list-item">
+        <h3 class="titre">Terminé</h3>
+        <p class="desc"><span class="color"><?= $nbr_termine_week ?></span> pré-admissions terminé pour cette semaine.</p>
+    </a>
+
+    <a href="ajout_admission" class="list-item">
+        <h3 class="titre">Créer une nouvelle pré-admission</h3>
+    </a>
+</ul>
