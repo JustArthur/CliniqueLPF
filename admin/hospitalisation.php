83eea37673de6de8939284d682e4d5cea6eab123
su apt install kuma <maxxozou@gmail.com>
2023-03-31 08:58:48 +0200

    Ver 1.26.1

--- admin/hospitalisation.php
+++ admin/hospitalisation.php
@@ -1,4 +1,6 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
     include_once('../include.php');
 
     if(empty($_SESSION['utilisateur'][5]) || $_SESSION['utilisateur'][3] != 1) {
